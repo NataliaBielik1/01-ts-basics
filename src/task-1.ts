ffe217a5eee7f2c8d078b18e8d385266ab8ee89d
Nataliia Bielik <natalia.belic2607@gmail.com>
2025-07-23 23:45:29 -0400

    add files

---
+++ src/task-1.ts
@@ -0,0 +1,7 @@
+const name: { name: string } = "Alice";
+const age: {age: number }= 30;
+const isOnline: {isOnline: boolean }= true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
